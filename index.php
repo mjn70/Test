275da362b89995d715c6dde6c49e93188309d44a
mjn70 <mj.mosa@gmail.com>
2017-01-10 22:12:49 +0300

    add HTML Codes

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-<php 
 
-	echo "Hello World!";
+<html> 
 
+    <head>
+        
+        <?php 
+
+	echo "<h1> Hello World! <h1>";
+        echo "<br>";
 ?>
+        
+    </head>
+    
+    
+    
+    <body  > 
+    
+        <div center: >
+            
+            
+            
+        </div>
+    
+    </body>
+
+
+</html>
